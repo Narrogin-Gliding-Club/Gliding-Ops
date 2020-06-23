186f30b2f93118897850bfb0e9b9c135a8f50c29
Peter F Bradshaw <pfb@exadios.com>
2020-06-23 13:57:26 +0800

    Build: Moved repository
    Moved from "Narrogin-Gliding-Club to NGC.

--- LogKeeper/PreambleProcessing/APRS/APRS-client.sh
+++ LogKeeper/PreambleProcessing/APRS/APRS-client.sh
@@ -6,7 +6,7 @@ then
   exit 255
 fi
 
-BASE=${HOME}/Work/Narrogin-Gliding-Club/Gliding-Ops/LogKeeper
+BASE=${HOME}/Work/NGC/Gliding-Ops/LogKeeper
 EXECUTABLE_PATH=${BASE}/PreambleProcessing/APRS
 LIB_BASE=${BASE}/lib
 export PYTHONPATH=${PYTHONPATH}:${LIB_BASE}/python-ogn-client
