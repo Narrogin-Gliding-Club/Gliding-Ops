41925470eea8ae7b829f9ba7cd10e171d0e3002e
Peter F Bradshaw <pfb@exadios.com>
2020-07-11 07:42:20 +0800

    APRS-killer: Added per station capability

--- LogKeeper/PreambleProcessing/APRS/APRS-killer.sh
+++ LogKeeper/PreambleProcessing/APRS/APRS-killer.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]
+then
+  echo ${0} "<filter from filter.txt>"
+  exit 255
+fi
+
 BASE=${HOME}/Work/NGC/Gliding-Ops/LogKeeper
 EXECUTABLE_PATH=${BASE}/PreambleProcessing/APRS
 LIB_BASE=${BASE}/lib
 export PYTHONPATH=${PYTHONPATH}:${LIB_BASE}/python-ogn-client
 
+grep -l ${1} ${EXECUTABLE_PATH}/filters.txt > /dev/null
+if [ $? -ne 0 ]
+then
+  echo "${0}: Filter ${1} not found"
+  exit 1
+fi
+station=$(cat ${EXECUTABLE_PATH}/filters.txt | grep "^${1}" | cut -d ':' -f 1)
+
 file_base=$(date "+%Y-%m-%d")
-DATA_BASE=${BASE}/Data
-for station in $(ls -d *)
+data_path=${BASE}/Data/${station}/APRS
+for locker in $(ls ${data_path}/*.lock 2> /dev/null)
 do
-  if [ -d ${station} ]
-  then
-    data_path=${DATA_BASE}/${station}/APRS
-    for locker in $(ls ${data_path}/*.lock 2> /dev/null)
-    do
-      kill -SIGINT $(cat ${locker})
-      kill -SIGHUP $(cat ${locker})
-      rm ${locker}
-    done
-  fi
+  kill -SIGINT $(cat ${locker})
+  kill -SIGHUP $(cat ${locker})
+  rm ${locker}
 done
